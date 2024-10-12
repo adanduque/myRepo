c9d2f8e5d4748d3abd5dc6aa0f7e24cec4bc8b24
adanduque <smith.asda87@gmail.com>
2024-10-12 16:31:10 -0500

    Init Project

---
+++ src/main/java/com/mservPolizas/mservPolizas/MservPolizasApplication.java
@@ -0,0 +1,13 @@
+package com.mservPolizas.mservPolizas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MservPolizasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MservPolizasApplication.class, args);
+	}
+
+}
